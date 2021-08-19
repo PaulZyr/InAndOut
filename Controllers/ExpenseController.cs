9b735d5769b0fa8efedf67bf00b834617916d22d
paulzyr <pavel.ziryanov@ithoot.com>
2021-08-19 18:30:23 +0300

    view bag

--- Controllers/ExpenseController.cs
+++ Controllers/ExpenseController.cs
@@ -2,6 +2,7 @@
 using InAndOut.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -34,6 +35,14 @@ namespace InAndOut.Controllers
         // GET: ExpensesController/Create
         public ActionResult Create()
         {
+            IEnumerable<SelectListItem> TypeDropDown = _db.ExpenseTypes.Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            });
+
+            ViewBag.TypeDropDown = TypeDropDown;
+
             return View();
         }
 
